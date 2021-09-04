20e4749ef1e72644e3a95ee653737dede3e7ee41
Sami Dahoux <samidahoux@gmail.com>
2021-09-04 16:28:14 +0200

    feat: adapting logger to both prod and dev

--- src/utils/logging.js
+++ src/utils/logging.js
@@ -1,19 +1,34 @@
 import winston from "winston";
 import { performance, PerformanceObserver } from "perf_hooks";
 
-const logLevel = process.env.NODE_ENV !== "production" ? "debug" : "info";
+function createLogger() {
+    const isNotProduction = process.env.NODE_ENV !== "production";
+    const { combine, timestamp } = winston.format;
+    const levels = ["warn", "error"];
+    const logLevel = isNotProduction ? "debug" : "info";
+
+    const transports = isNotProduction
+        ? [new winston.transports.Console(), new winston.transports.File({ filename: "api.log" })]
+        : [new winston.transports.Console()];
+
+    const logging = isNotProduction
+        ? ({ level, message, timestamp }) => {
+              return `${timestamp.slice(0, -5)} | ${level.toUpperCase().padEnd(7)}| ${message}`;
+          }
+        : ({ level, message }) => {
+              return `${level.toUpperCase().padEnd(7)}| ${message}`;
+          };
+    return winston.createLogger({
+        level: logLevel,
+        format: combine(timestamp(), winston.format.printf(logging)),
+        transports: transports,
+        stderrLevels: levels,
+    });
+}
 
 // Logger setup
 
-const { combine, timestamp } = winston.format;
-const loggingFormat = winston.format.printf(({ level, message, timestamp }) => {
-    return `${timestamp.slice(0, -5)} | ${level.toUpperCase().padEnd(7)}| ${message}`;
-});
-export const logger = winston.createLogger({
-    level: logLevel,
-    format: combine(timestamp(), loggingFormat),
-    transports: [new winston.transports.Console()],
-});
+export const logger = createLogger();
 
 // Performance observation setup
 
