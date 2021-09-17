3d0ad7496ebe2e106d060128620400858c2a2f39
Sami Dahoux <samidahoux@gmail.com>
2021-09-17 19:44:06 +0200

    fix: jwt authentication signature

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,49 +1,77 @@
 import express from "express";
 import { connectToDb } from "../db/index.js";
 import { logger, performance } from "../utils/logging.js";
-import crypto from "crypto";
 import { DbConfig, DbUser } from "../config.js";
 import { updateUser } from "../db/queries.js";
+import jwt from "jsonwebtoken";
 
-const generateAuthToken = () => {
-    return crypto.randomBytes(30).toString("hex");
+const generateAccessToken = (username) => {
+    return jwt.sign(username, process.env.JWT_ACCESS_SECRET);
 };
 
-function generateAuthUser(tokens, config) {
-    return async function authenticateUser(req, res) {
+const getAccessToken = (req) => {
+    const authorization = req.headers["authorization"];
+    return authorization && authorization.split(" ")[1];
+};
+
+function generateLogUser(tokens, config) {
+    return async function logUser(req, res) {
         const { username, password } = req.body;
         const user = new DbUser(username, password);
         const db = new DbConfig(config.db.host, config.db.port, config.db.name, user);
         try {
             performance.mark("authUserStart");
             const client = await connectToDb(db);
-            const authToken = generateAuthToken();
-            tokens[authToken] = user;
+            const token = generateAccessToken(user.username);
+            tokens[token] = user;
             client.close();
             performance.mark("authUserEnd");
             logger.info(`Authentication of "${user.username}" succeeded`);
             performance.measure("authUserPerf", "authUserStart", "authUserEnd");
-            res.cookie("AuthToken", authToken).sendStatus(200);
+            console.log(token);
+            res.status(200).send(token);
         } catch (error) {
             logger.info(`Authentication of "${user.username}" failed`);
             logger.warn(error.message);
             if (error.reason) {
                 logger.debug(error.reason.message);
             }
-            res.sendStatus(403);
-            return;
+            return res.sendStatus(403);
         }
+    };
+}
+
+function generateAuthUser(tokens) {
+    return function authenticateUser(req, res, next) {
+        const token = getAccessToken(req);
+        console.log(req.headers);
+        if (!token) {
+            return res.sendStatus(401);
+        }
+        jwt.verify(token, process.env.JWT_ACCESS_SECRET, (error, user) => {
+            if (error) {
+                logger.warn(error.toString());
+                return res.sendStatus(403);
+            }
+            if (user !== tokens[token].username) {
+                logger.warn(`Unauthenticated user "${user}" trying to access admin`);
+                return res.sendStatus(401);
+            }
+            next();
+        });
+    };
+}
+
+function generateCheckAuthUser() {
+    return function authenticateUser(req, res) {
+        res.sendStatus(200);
     };
 }
 
 function generateEditUser(tokens, config) {
     return async function editUser(req, res) {
-        const user = tokens[req.cookies["AuthToken"]];
-        if (!user) {
-            logger.warn(`Unauthenticated user attempting to edit`);
-            res.status(403).send("Access forbidden!");
-            return;
-        }
+        const token = getAccessToken(req);
+        const user = tokens[token];
         logger.info(`Edition requested by ${user.username}`);
         performance.mark("editUserStart");
         const db = new DbConfig(config.db.host, config.db.port, config.db.name, user);
@@ -55,6 +83,8 @@ function generateEditUser(tokens, config) {
             performance.measure("editUserPerf", "editUserStart", "editUserEnd");
         } catch (error) {
             logger.warn(error.message);
+            res.sendStatus(500);
+            return;
         }
         res.sendStatus(200);
     };
@@ -62,7 +92,9 @@ function generateEditUser(tokens, config) {
 
 export default function getAdminRoutes(tokens, config) {
     const router = express.Router();
-    router.post("/auth", generateAuthUser(tokens, config));
+    router.post("/login", generateLogUser(tokens, config));
+    router.use(generateAuthUser(tokens));
+    router.post("/auth", generateCheckAuthUser());
     router.post("/edit", generateEditUser(tokens, config));
     return router;
 }
